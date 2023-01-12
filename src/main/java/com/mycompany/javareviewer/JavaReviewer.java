97d4247174104dc9d617ab2441e1b59a99759c34
alanger <mightydodongo@gmail.com>
2023-01-12 17:05:28 -0600

    Methods

--- src/main/java/com/mycompany/javareviewer/JavaReviewer.java
+++ src/main/java/com/mycompany/javareviewer/JavaReviewer.java
@@ -7,12 +7,15 @@ package com.mycompany.javareviewer;
 import com.github.javaparser.JavaParser;
 import static com.github.javaparser.StaticJavaParser.parse;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JFileChooser;
@@ -23,6 +26,66 @@ import javax.swing.JFileChooser;
  */
 public class JavaReviewer {
     private final int ultraman = 2;
+    
+    public static class MethodInfo {
+        private String className;
+        private String methodName;
+        private int lineNumber;
+
+        public MethodInfo(String className, String methodName, int lineNumber) {
+            this.className = className;
+            this.methodName = methodName;
+            this.lineNumber = lineNumber;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        public String getMethodName() {
+            return methodName;
+        }
+
+        public int getLineNumber() {
+            return lineNumber;
+        }
+    }
+
+    public static List<MethodInfo> findMethods(String filePath) {
+        List<MethodInfo> methodList = new ArrayList<>();
+        try {
+            // Parse the file
+            FileInputStream in = new FileInputStream(filePath);
+            CompilationUnit cu = parse(in);
+            in.close();
+
+            // Visit and print the methods names and line numbers
+            cu.accept(new MethodVisitor(methodList), null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return methodList;
+    }
+
+    private static class MethodVisitor extends VoidVisitorAdapter<Void> {
+        private List<MethodInfo> methodList;
+
+        public MethodVisitor(List<MethodInfo> methodList) {
+            this.methodList = methodList;
+        }
+
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            // Get the class name
+            String className = n.findAncestor(ClassOrInterfaceDeclaration.class).get().getNameAsString();
+            // Get the method name
+            String methodName = n.getNameAsString();
+            // Get the line number
+            int lineNumber = n.getBegin().get().line;
+            methodList.add(new MethodInfo(className, methodName, lineNumber));
+        }
+    }
+    
     
     public static class VariableInfo {
         public String className;
@@ -112,6 +175,12 @@ public class JavaReviewer {
                 
                 for (VariableInfo variable : variables) {
                     System.out.println(variable);
+                }
+                
+                List<MethodInfo> methods = findMethods(path);
+                
+                for (MethodInfo method : methods) {
+                    System.out.println("METHOD: " + method.getClassName() + ", " + method.getMethodName() + ", " + method.getLineNumber());
                 }
             }
         }
